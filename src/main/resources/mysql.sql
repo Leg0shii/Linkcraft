7677e4bdaf5d804889e38df5a8c9b3bf9b0da98d
Dkay <dklink750@gmail.com>
2022-11-03 22:58:28 -0400

    Added tentative db table

--- src/main/resources/mysql.sql
+++ src/main/resources/mysql.sql
@@ -1,3 +1,14 @@
-CREATE TABLE `{p}players` (
-    `uuid`      VARCHAR(36) PRIMARY KEY
+CREATE TABLE IF NOT EXISTS `{p}players` (
+  `uuid`      VARCHAR(36) PRIMARY KEY
+);
+
+-- Chose MEDIUMINT because I doubt we will ever go over 8 million signs lol (feel free to change if you would like)
+-- This table will store signs/heads with right click events (rankup signs/teleport heads/segmented cps)
+-- Will likely have a foreign key to an effects table
+CREATE TABLE IF NOT EXISTS `{p}effect_blocks` (
+  `id`    MEDIUMINT AUTO_INCREMENT NOT NULL,
+  `world` CHAR(36)                 NOT NULL,
+  `x`     INT                      NOT NULL,
+  `y`     INT                      NOT NULL,
+  `z`     INT                      NOT NULL
 );
