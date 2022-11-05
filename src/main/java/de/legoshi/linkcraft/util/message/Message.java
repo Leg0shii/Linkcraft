798da1920a1d4426e72f6d6a69e7aa525b8d7e61
Dkay <dklink750@gmail.com>
2022-11-05 14:39:48 -0400

    Fixed static issue with Message enum

--- src/main/java/de/legoshi/linkcraft/util/message/Message.java
+++ src/main/java/de/legoshi/linkcraft/util/message/Message.java
@@ -6,10 +6,13 @@ import java.util.Objects;
 
 public enum Message {
 
-    PLAYER_JOIN("{0} joined the Server!"),
-    PLAYER_LEAVE("{0} left the Server!");
 
-    String m;
+
+    PLAYER_JOIN("{0} joined the Server!"),
+    PLAYER_LEAVE("{0} left the Server!"),
+    CMD_COOLDOWN("Please wait {0} second(s) before your next command.");
+
+    final String m;
 
     Message(String message) {
         this.m = message;
@@ -17,29 +20,34 @@ public enum Message {
 
 
     public String msg() {
-        this.m = ChatColor.translateAlternateColorCodes('&', m);
-        return this.m;
+        return colourize(this.m);
     }
 
     public String msg(String... args) {
-
-        for (int i = 0; i < args.length; i++) {
-            String replacement = args[i];
-            this.m = this.m.replace("{" + i + "}", Objects.nonNull(replacement) ? replacement : "<?>");
-        }
-
-        return this.m;
+        return template(this.m, args);
     }
 
     public String msg(Prefix prefix) {
-        this.m = prefix.text() + this.m;
-        return msg();
+        return colourize(prefix.text() + this.m);
     }
 
     public String msg(Prefix prefix, String... args) {
-        this.m = msg(args);
-        this.m = msg(prefix);
-        return this.m;
+        return colourize(template(prefix.text() + this.m, args));
+    }
+
+
+    private String colourize(String message) {
+        return ChatColor.translateAlternateColorCodes('&', message);
+    }
+
+    private String template(String message, String... args) {
+        String result = message;
+
+        for (int i = 0; i < args.length; i++) {
+            String replacement = args[i];
+            result = message.replace("{" + i + "}", Objects.nonNull(replacement) ? replacement : "<?>");
+        }
+        return result;
     }
 
     public String getRawMessage() {
