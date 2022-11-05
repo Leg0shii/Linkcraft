34d6443e64d52376aa0c5a72f29b12fcdd8939db
Dkay <dklink750@gmail.com>
2022-11-04 20:09:58 -0400

    Removed prefixes from message enum

--- src/main/java/de/legoshi/linkcraft/util/message/Message.java
+++ src/main/java/de/legoshi/linkcraft/util/message/Message.java
@@ -6,42 +6,40 @@ import java.util.Objects;
 
 public enum Message {
 
-    INFO_PREFIX("&6&l[Linkcraft]&7 "),
-    ERR_PREFIX("&c[Error]&7 "),
-    SUC_PREFIX("&a[Success]&7 "),
-    NO_PREFIX("&7"),
-
     PLAYER_JOIN("{0} joined the Server!"),
     PLAYER_LEAVE("{0} left the Server!");
 
-    final String m;
+    String m;
 
     Message(String message) {
         this.m = message;
     }
 
-    public String msg(Prefix prefix) {
-        String manip = m;
 
-        if (prefix == Prefix.INFO) manip = INFO_PREFIX.m + manip;
-        else if (prefix == Prefix.ERROR)  manip = ERR_PREFIX.m + manip;
-        else if (prefix == Prefix.SUCCESS)  manip = SUC_PREFIX.m + manip;
-        else manip = NO_PREFIX.m + manip;
-
-        manip = ChatColor.translateAlternateColorCodes('&', manip);
-        return manip;
+    public String msg() {
+        this.m = ChatColor.translateAlternateColorCodes('&', m);
+        return this.m;
     }
 
-    public String msg(Prefix prefix, String... args) {
-        String manip = m;
+    public String msg(String... args) {
 
         for (int i = 0; i < args.length; i++) {
             String replacement = args[i];
-            manip = manip.replace("{" + i + "}", Objects.nonNull(replacement) ? replacement : "<?>");
+            this.m = this.m.replace("{" + i + "}", Objects.nonNull(replacement) ? replacement : "<?>");
         }
 
-        manip = msg(prefix);
-        return manip;
+        return this.m;
+    }
+
+    public String msg(Prefix prefix) {
+        this.m = prefix.text() + this.m;
+        return msg();
+    }
+
+    public String msg(Prefix prefix, String... args) {
+        this.m = msg(args);
+        this.m = msg(prefix);
+        return this.m;
     }
 
     public String getRawMessage() {
--- src/main/java/de/legoshi/linkcraft/util/message/Prefix.java
+++ src/main/java/de/legoshi/linkcraft/util/message/Prefix.java
@@ -2,9 +2,17 @@ package de.legoshi.linkcraft.util.message;
 
 public enum Prefix {
 
-    NONE,
-    INFO,
-    ERROR,
-    SUCCESS
+    INFO("&6&l[Linkcraft]&7 "),
+    ERROR("&c[Error]&7 "),
+    SUCCESS("&a[Success]&7 ");
 
+    private final String p;
+
+    public String text() {
+        return p;
+    }
+
+    Prefix(String prefix) {
+        this.p = prefix;
+    }
 }
