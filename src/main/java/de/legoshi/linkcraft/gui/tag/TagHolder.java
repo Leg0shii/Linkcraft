6e0b89f3c534601b4a7f5e9c8138dc9b18a8f213
0hnezahn <benjamin.mueller2@uni-rostock.de>
2022-11-10 02:00:50 +0100

    add tag gui

---
+++ src/main/java/de/legoshi/linkcraft/gui/tag/TagHolder.java
@@ -0,0 +1,35 @@
+package de.legoshi.linkcraft.gui.tag;
+
+import de.legoshi.linkcraft.Linkcraft;
+import de.legoshi.linkcraft.gui.GUIPane;
+import de.legoshi.linkcraft.gui.GUIScrollable;
+import de.legoshi.linkcraft.tag.TagRarity;
+import de.themoep.inventorygui.InventoryGui;
+import org.bukkit.entity.Player;
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class TagHolder extends GUIScrollable {
+
+    private final String title;
+    private final String[] guiSetup = {
+            "ggggggggu",
+            "ggggggggf",
+            "ggggggggq",
+            "ggggggggf",
+            "ggggggggd",
+    };
+
+    public TagHolder(Player player, InventoryGui parent, TagRarity rarity) {
+        super(player, parent);
+        this.title = rarity.name() + " tags";
+        this.current = new InventoryGui((JavaPlugin) Linkcraft.getPlugin(), player, title, guiSetup);
+        registerGuiElements();
+    }
+
+    @Override
+    protected void registerGuiElements() {
+        // load in all tags for the category from the player
+        this.current.addElements(this.pageUp, this.pageDown, this.returnToParent);
+    }
+
+}
